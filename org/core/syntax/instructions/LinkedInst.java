8a354787fb0927968ca2eaf001cada2d83904946
XAMEUS <xam.master.3d@gmail.com>
2015-04-19 12:08:52 +0200

    LinkedInst
    LinkedList system of instructions

---
+++ org/core/syntax/instructions/LinkedInst.java
@@ -0,0 +1,21 @@
+package org.core.syntax.instructions;
+
+import org.core.syntax.Instruction;
+
+public class LinkedInst extends Instruction {
+
+	private Instruction current;
+	private LinkedInst next;
+	
+	public LinkedInst(Instruction current, LinkedInst next) {
+		this.current = current;
+		this.next = next;
+	}
+	
+	@Override
+	public void exec() throws Exception {
+		this.current.exec();
+		if (this.next != null)
+			this.next.exec();
+	}
+}
