f842b993fd0b6a650a7721d9b2ec0dc7ec3ffc8c
XAMEUS <xam.master.3d@gmail.com>
2015-04-22 08:52:31 +0200

    Code organization

---
+++ org/ui/editor/GTextEditorFX.java
@@ -0,0 +1,110 @@
+package org.ui.editor;
+
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
+
+import org.fonts.Fonts;
+
+public class GTextEditorFX extends TabPane {
+	
+	private GEditorFX gefx;
+	
+	private class TextTab extends Tab {
+		
+		private TextArea textArea;
+		
+		public TextTab(String text) {
+			
+			this.setText(text);
+			
+			this.textArea = new TextArea();
+			this.textArea.getStyleClass().add("console");
+			this.textArea.setFont(Font.loadFont(Fonts.class.getResource("LiberationMono-Regular.ttf").toString(), 12));
+			
+			this.createActions();
+
+			BorderPane content = new BorderPane();
+			content.setCenter(this.textArea);
+			this.setContent(content);
+		}
+		
+		private void createActions() {
+			
+			this.textArea.setOnKeyReleased(new EventHandler<Event>() {
+				@Override
+				public void handle(Event event) {
+					gefx.drawInfos();
+				}
+			});
+			
+			this.textArea.setOnMouseClicked(new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(MouseEvent event) {
+					gefx.drawInfos();
+				}
+			});
+			
+		}
+		
+		public TextArea getTextArea() {
+			return this.textArea;
+		}
+		
+	}
+	
+	public GTextEditorFX(GEditorFX gefx) {
+		this.gefx = gefx;
+		this.newTab();
+		this.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				gefx.drawInfos();
+			}
+		});
+	}
+	
+	public void newTab() {
+		this.newTab("New File");
+	}
+	
+	public void newTab(String name) {
+
+		Tab tab = new TextTab(name);
+		this.getTabs().add(tab);
+		
+	}
+	
+	public void setTabName(String name) {
+		for (Tab t : this.getTabs())
+			if (t.isSelected())
+				t.setText(name);
+	}
+	
+	public String getText() {
+		for (Tab t : this.getTabs())
+			if (t.isSelected())
+				return ((TextTab)t).getTextArea().getText();
+		return null;
+	}
+	
+	public int getCursorPosition() {
+		for (Tab t : this.getTabs())
+			if (t.isSelected())
+				return ((TextTab)t).getTextArea().getCaretPosition();
+		return 0;
+	}
+	
+	public TextArea getTextArea() {
+		for (Tab t : this.getTabs())
+			if (t.isSelected())
+				return ((TextTab)t).getTextArea();
+		return null;
+	}
+	
+}
---
+++ org/ui/editor/GTextInfosFX.java
@@ -0,0 +1,41 @@
+package org.ui.editor;
+
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.text.TextAlignment;
+
+public class GTextInfosFX extends Canvas {
+	
+	private double width;
+	
+	public GTextInfosFX(double w, String text, int pos) {
+		this.width = w;
+		this.setWidth(w);
+		this.setHeight(20);
+		this.draw(text, pos);
+	}
+	
+	public void draw(String t, int pos) {
+		
+		GraphicsContext gc = this.getGraphicsContext2D();
+		gc.setFill(new Color(0, 0, 0, 1));
+		
+		int l = 0;
+		int c = 0;
+		for (int i = 0; i < pos; i++) {
+			if(t.charAt(i) == '\n') {
+				l += 1;
+				c = 0;
+			}
+			else c++;
+		}
+		
+		gc.setFill(new Color(0.4, 0.4, 0.4, 1));
+		gc.setTextAlign(TextAlignment.RIGHT);
+		gc.fillText("chars: " + t.length() + "  |  lines: " + t.split("\r\n|\r|\n").length, this.width - 5, 14, this.width);
+		gc.setTextAlign(TextAlignment.LEFT);
+		gc.fillText("line: " + l + "  |  col: " + c + "  |  char: " + pos , 5, 14, this.width);
+	}
+
+}
