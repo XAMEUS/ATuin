c37ae7fe40089836ad204874e72369b9c7a5bb29
XAMEUS <xam.master.3d@gmail.com>
2015-04-25 09:39:11 +0200

    TabPaneWrapper Class

---
+++ org/ui/TabPaneWrapper.java
@@ -0,0 +1,25 @@
+package org.ui;
+
+import javafx.geometry.Orientation;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.SplitPane;
+
+class TabPaneWrapper {
+	
+	private SplitPane split;
+
+	public TabPaneWrapper(Orientation o, double splitLocation) {
+		split = new SplitPane();
+		split.setOrientation(o);
+		split.setDividerPosition(0, splitLocation);
+	}
+
+	public void addNodes(final Node node1, final Node node2) {
+		split.getItems().addAll(node1, node2);
+	}
+
+	public Parent getNode() {
+		return split;
+	}
+}
