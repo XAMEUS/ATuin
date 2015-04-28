331d542563dbda82e07ca2b93f6029a1bec3f9dc
XAMEUS <xam.master.3d@gmail.com>
2015-04-28 23:52:13 +0200

    New instructions

---
+++ org/core/syntax/instructions/Down.java
@@ -0,0 +1,12 @@
+package org.core.syntax.instructions;
+
+import org.core.syntax.Instruction;
+
+public class Down extends Instruction {
+
+	@Override
+	public void exec() throws Exception {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ org/core/syntax/instructions/Turn.java
@@ -0,0 +1,12 @@
+package org.core.syntax.instructions;
+
+import org.core.syntax.Instruction;
+
+public class Turn extends Instruction {
+
+	@Override
+	public void exec() throws Exception {
+		// TODO Auto-generated method stub
+	}
+
+}
