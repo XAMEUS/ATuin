5434ebdc71c1e8d0382f3fa8ca1ba9a2f5e6e28c
XAMEUS <xam.master.3d@gmail.com>
2015-04-29 20:37:27 +0200

    Or eval

--- org/core/syntax/expressions/Or.java
+++ org/core/syntax/expressions/Or.java
@@ -4,9 +4,16 @@ import org.core.syntax.Expression;
 
 public class Or extends Expression {
 
+	private Expression left, right;
+	
+	public Or(Expression left, Expression right) {
+		this.left = left;
+		this.right = right;
+	}
 	@Override
 	public int eval() throws Exception {
-		// TODO Auto-generated method stub
+		if (this.left.eval() == 1 || this.right.eval() == 1)
+			return 1;
 		return 0;
 	}
 
