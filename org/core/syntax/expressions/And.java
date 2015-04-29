cc2c2d41dccbbded76c7e77413ee0eefbd09dd81
XAMEUS <xam.master.3d@gmail.com>
2015-04-29 20:36:56 +0200

    And eval

--- org/core/syntax/expressions/And.java
+++ org/core/syntax/expressions/And.java
@@ -4,9 +4,17 @@ import org.core.syntax.Expression;
 
 public class And extends Expression {
 
+	private Expression left, right;
+	
+	public And(Expression left, Expression right) {
+		this.left = left;
+		this.right = right;
+	}
+	
 	@Override
 	public int eval() throws Exception {
-		// TODO Auto-generated method stub
+		if (this.left.eval() == 1 && this.right.eval() == 1)
+			return 1;
 		return 0;
 	}
 
