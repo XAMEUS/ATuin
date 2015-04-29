f36d623afcd4f057cae1ba8878a8a58e077cd6bb
XAMEUS <xam.master.3d@gmail.com>
2015-04-29 20:37:19 +0200

    Inf eval

--- org/core/syntax/expressions/Inf.java
+++ org/core/syntax/expressions/Inf.java
@@ -2,12 +2,24 @@ package org.core.syntax.expressions;
 
 import org.core.syntax.Expression;
 
-public class Inf extends Expression{
+public class Inf extends Expression {
 
+	private Expression left, right;
+	private boolean strict;
+	
+	public Inf(Expression left, Expression right, boolean strict) {
+		this.left = left;
+		this.right = right;
+		this.strict = strict;
+	}
+	
 	@Override
 	public int eval() throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		if (this.left.eval() > this.right.eval())
+			return 0;
+		if (this.strict && this.left.eval() == this.right.eval())
+			return 0;
+		return 1;
 	}
 
 }
