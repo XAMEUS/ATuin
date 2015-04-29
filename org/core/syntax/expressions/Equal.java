f77473d9d76bc962fd72578b0f7df5890c3a51fa
XAMEUS <xam.master.3d@gmail.com>
2015-04-29 20:37:12 +0200

    Equal eval

--- org/core/syntax/expressions/Equal.java
+++ org/core/syntax/expressions/Equal.java
@@ -4,9 +4,17 @@ import org.core.syntax.Expression;
 
 public class Equal extends Expression {
 
+	private Expression left, right;
+	
+	public Equal(Expression left, Expression right) {
+		this.left = left;
+		this.right = right;
+	}
+	
 	@Override
 	public int eval() throws Exception {
-		// TODO Auto-generated method stub
+		if (this.left.eval() == this.right.eval())
+			return 1;
 		return 0;
 	}
 
