b258a2dd716d33ffc0b169345ad0cf38d406c8fa
XAMEUS <xam.master.3d@gmail.com>
2015-04-30 17:22:42 +0200

    Auto-scroll improvement

--- org/ui/console/GConsoleFX.java
+++ org/ui/console/GConsoleFX.java
@@ -1,11 +1,13 @@
 package org.ui.console;
 
-import org.fonts.Fonts;
-
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Font;
+
+import org.fonts.Fonts;
 
 public class GConsoleFX extends Tab {
 	
@@ -22,16 +24,22 @@ public class GConsoleFX extends Tab {
 		bp.setCenter(textArea);
 		this.setContent(bp);
 		
+		textArea.textProperty().addListener(new ChangeListener<Object>() {
+		    @Override
+		    public void changed(ObservableValue<?> observable, Object oldValue,
+		            Object newValue) {
+		        textArea.setScrollTop(Double.MAX_VALUE);
+		    }
+		});
+		
 	}
 	
 	public void print(String s) {
-		textArea.setText(textArea.getText()+s);
-		textArea.setScrollTop(Double.MAX_VALUE);
+		textArea.appendText(s);
 	}
 	
 	public void println(String s) {
-		textArea.setText(textArea.getText()+s+"\n");
-		textArea.setScrollTop(Double.MAX_VALUE);
+		textArea.appendText(s + "\n");
 	}
 	
 }
