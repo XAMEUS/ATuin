5b2fe7909ce653194d5f7f0e32ba45a4af249035
XAMEUS <xam.master.3d@gmail.com>
2015-05-04 22:36:25 +0200

    Tests

--- org/core/tests/NumberCoreTest.java
+++ org/core/tests/NumberCoreTest.java
@@ -8,5 +8,6 @@ public class NumberCoreTest {
 		Number b = new Number(4);
 		java.lang.Number c = 4;
 		System.out.println(a.equals(b));
+		System.out.println(new Number(2).inferior(new Number(10)));
 	}
 }
