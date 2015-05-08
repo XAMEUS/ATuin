1681641217df82d6f5e5ffccffc9a8e6253f6059
XAMEUS <xam.master.3d@gmail.com>
2015-05-08 12:26:40 +0200

    Exec method

--- org/core/syntax/instructions/Up.java
+++ org/core/syntax/instructions/Up.java
@@ -1,12 +1,13 @@
 package org.core.syntax.instructions;
 
 import org.core.syntax.Instruction;
+import org.ui.controller.TurtleBrain;
 
 public class Up implements Instruction {
 
 	@Override
 	public void exec() throws Exception {
-		// TODO Auto-generated method stub
+		TurtleBrain.up();
 	}
 
 }
